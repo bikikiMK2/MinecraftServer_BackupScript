6472b94a35995007af51c35310493bacb415a0b9
bikikiMK2 <hibiki@bikikimk2.net>
2025-04-18 21:27:49 +0900

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ CURRENT_DATE=$(date +%Y-%m-%d-%H%M) # be this date format -> 2025-01-11-1234
 SCREEN_NAME= # set screen session name here
 SERVER_DIR= # set Server dir pass
 WORLD_DIR= # set Server World dir pass
-BUCKUP_DIR= # backuped files are save this dir
+BACKUP_DIR= # backuped files are save this dir
 ZIPNAME=world{$CURRENT_DATE}.zip # set name as zipped world data
 
 set -e
@@ -17,6 +17,6 @@ echo Saving World Data... # to command-rine
 screen -RdS $SCREEN_NAME -X stuff 'say §dSaving World Data...'`echo -ne '\015'` # to SCREEN_NAME
 zip -q -r $SERVER_DIR/$ZIPNAME $WORLD_DIR # archiving to zip then this here
 
-mv $ZIPNAME $BUCKUP_DIR # move zipped file to BUCKUP_DIR from SERVER_DIR
+mv $ZIPNAME $BACKUP_DIR # move zipped file to BACKUP_DIR from SERVER_DIR
 echo Backup Process is Done # to command-rine
 screen -RdS $SCREEN_NAME -X stuff 'say §dServer Backup is Done!'`echo -ne '\015'` # to SCREEN_NAME
